605d76012afe7e054640956f4ee79d04c74e75b4
the-last <1039107092@qq.com>
2019-10-23 11:13:36 +0800

    python质数计算优化

--- Python/code/7.练习1质数.py
+++ Python/code/7.练习1质数.py
@@ -1,18 +1,37 @@
 # coding=UTF-8
 # 判断是否是质数，被1 和 它本身整除的数，就是质数。
+# 引入模块，可以对Python扩展，比如 time函数
 
-num = int(input('请输入一个大于1的整数：'));
+from time import *
+begin = time()
+print begin
 
-# 首先获取所有可能的因数
-i = 2
-prime = False;
-while i < num:
-    # 能否被 i整除
-    if num % i == 0:
-        prime = True;
-    i += 1
+num2 = int(input('请输入一个整数'))
 
-if prime:
-    print num, 'is prime'
+# 第一次优化在while 循环加break，找到一个引子就break了
+# 第二次优化，减少while 循环的次数，比如37，没必要把37之前的所有数都去整除37来判断是质数，比较到37**0.5就能证明37是质数。
+# 36是合数
+# 因子有：
+# 2 18
+# 3 12
+# 4 9 
+# 6 6
+# 
+i1 = 2
+while i1 < num2:
+    flag = True
+    j = 2
+    while j <= i1 ** 0.5:
+        if i1 % j == 0:
+            flag = False
+            break
+        j += 1
+    if flag:
+        print i1
+    i1 += 1
 else:
-    print num, 'is not prime'
+    print i1
+
+end = time()
+print end
+print end - begin
