7d899099f0b1ce1edbea229d98bf9f946fc9c096
the-last <1039107092@qq.com>
2020-01-30 13:24:38 +0800

    使用自定义pack工具打包

---
+++ Webpack/webpackxxx/src/common/util.js
@@ -0,0 +1,3 @@
+module.export = age => {
+    console.log(`你今年${age}岁了`)
+}
---
+++ Webpack/webpackxxx/src/index.js
@@ -0,0 +1,5 @@
+const sayHello = require('./a.js')
+const sayAge   = require('./common/util.js')
+
+sayHello('大圣开课吧')
+sayAge(28)
